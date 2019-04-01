7bbc9c9d864f650f183007acbebb8054baaef769
kostis <kostis@pop-os.localdomain>
2019-04-01 20:43:45 +0300

    Title - in material -

--- homefinder/src/Components/TenantPage/tenant.js
+++ homefinder/src/Components/TenantPage/tenant.js
@@ -1,65 +1,49 @@
-import React from "react";
+import React, {Fragment} from "react";
 import Header from '../header';
-import './tenant.css';
+import './tenant.scss';
 import { Link } from 'react-router-dom';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
 
+const styles= {
+  paper:{padding:20, margin:20, textAlign:'center'},
+  text:{color:'#000', fontWeight:600}
+}
 class Tenant extends React.Component{
   render(){
-    return(
-      <div>
-        <Header/>
-        <h1 className="results">Find a Property</h1>
-          <div className="box">
-            <div className="filtersSearch">
-              <div className="customSelectSearch" >
-                <label>Price</label>
-                <select className="singleFilterSearch">
-                  <option selected disabled>--Price--</option>
-                  <option value="0-100$">0-100$</option>
-                  <option value="101-250$">101-250$</option>
-                  <option value="251-450$">251-450$</option>
-                  <option value="451$+">451$+</option>
-                </select>
-              </div>
-              <div className="customSelectSearch">
-                <label>Size(cm2)</label>
-                <select className="singleFilterSearch">
-                  <option selected disabled>--Size--</option>
-                  <option value="0-30cm2">0-30cm2</option>
-                  <option value="31-70cm2">31-70cm2</option>
-                  <option value="71-200cm2">71-200cm2</option>
-                  <option value="201cm2+">201cm2+</option>
-                </select>
-              </div>
-              <div className="customSelectSearch">
-                <label>Location</label>
-                <select className="singleFilterSearch">
-                  <option selected disabled>--Location--</option>
-                  <option value="Kamara">Kamara</option>
-                  <option value="Faliro">Faliro</option>
-                  <option value="Neapoli">Neapoli</option>
-                  <option value="Kalamaria">Kalamaria</option>
-                </select>
-              </div>
-            </div>
-            <div className="reason">
-              <div className="status">
-                <label class="statusBox">Buy
-                  <input type="checkbox"/>
-                    <span class="checkmark"></span>
-                </label>
-                <label class="statusBox">Rent
-                  <input type="checkbox"/>
-                    <span class="checkmark"></span>
-                </label>
-              </div>
-              <div>
-                <Link to="/results" className="textdecor"><h2 className="submit">Submit</h2></Link>
-              </div>
-            </div>
-          </div>
-        </div>
+    return(<Fragment>
+      <Header/>
+        <Grid container spacing={24}>
+
+        {/* Title */}
+          <Grid item xs={12} justify='center' alignItems="center">
+            <Paper style={styles.paper} elevation={0} >
+            <Typography variant="display3" style={styles.text}>
+                Submit Your Property
+            </Typography>
+            </Paper>
+          </Grid>
+
+          {/* Left Panel */}
+
+          <Grid item xs={6}>
+          <Paper>xs=6</Paper>
+        </Grid>
+       
+
+
+           {/* Right Panel */}
+
+           <Grid item xs={6}>
+          <Paper>xs=6</Paper>
+        </Grid>
+
+        </Grid> 
+        {/* Grid Container Closing */}
+    </Fragment>
+    
     )
   }
 }
