d3e4d52e90686600378f439eb5797fa663541808
kostis <kostis@pop-os.localdomain>
2019-05-29 12:56:18 +0300

    Phone textfield

--- homefinder/src/Components/LandlordPage/SubmitForm.js
+++ homefinder/src/Components/LandlordPage/SubmitForm.js
@@ -46,10 +46,12 @@ class SimpleSelect extends React.Component {
       sizeSubmit:sessionStorage.getItem('sizeSubmit'),
       locationSubmit:sessionStorage.getItem('locationSubmit'),
       statusSubmit:sessionStorage.getItem('statusSubmit'),
+      phoneSubmit:sessionStorage.getItem('phoneSubmit')
     }
 
     this.handleSizeChange = this.handleSizeChange.bind(this);
     this.handlePriceChange = this.handlePriceChange.bind(this);
+    this.handlePhoneNumber = this.handlePhoneNumber.bind(this);
   }
 
 
@@ -72,10 +74,28 @@ class SimpleSelect extends React.Component {
      }else{
        window.sessionStorage.setItem('locationSubmit', this.state.locationSubmit);
      }
+     if(this.state.phoneSubmit===null){
+      window.sessionStorage.setItem('phoneSubmit', '');
+    }else{
+      window.sessionStorage.setItem('phoneSubmit', this.state.phoneSubmit);
+    }
   }
 
 
+handlePhoneNumber(e){
+  let phoneValue = e.target.value;
+  this.setState({
+    phoneSubmit: phoneValue
+  },() =>{
+    if(this.state.phoneValue===null){
+      window.sessionStorage.setItem('phoneSubmit', '');
+    }else{
+      window.sessionStorage.setItem('phoneSubmit', this.state.phoneSubmit);
+    }
 
+    console.log(this.state.phoneSubmit)
+  });
+}
 
 handleSizeChange(e) {
   let sizeValue = e.target.value;
@@ -106,54 +126,6 @@ handlePriceChange(e) {
     console.log(this.state.priceSubmit)
   });
 }
-
-
-
-//   handleSizeChange(event) {
-//     console.log("Value from event:", event.target.value);
-
-//     this.setState({
-//       size: event.target.value
-//     }(), () => {
-//       console.log("New state in ASYNC callback:", this.state.size);
-//     });
-
-//     console.log("New state DIRECTLY after setState:", this.state.size);
-//   };
-// ;
-
-
-
-
-  // handleSizeChange(event){
-  //   event.preventDefault();
-  //   alert('Your current size is' +  this.input.value)
-  //   this.setState({size:event.target.value})
-  // }
-// handleSize(event){
-//   console.log('Value from event:');
-//   this.setState({
-//     size:event.target.value
-//   },()=>{
-//     console.log('New state in Async callback:', event.target.value);
-//   });
-//   console.log('New state DIRECTLY after the setState', this.state.size);
-// }
-
-
-
-
-  // handleSize(value){
-  //   this.setState({
-  //     size:value
-  //   });
-  //   if(this.state.size===null){
-  //     window.sessionStorage.setItem('size', '');
-  //   }else{
-  //     window.sessionStorage.setItem('size', this.state.size);
-  //   }
-  // }
-
   render() {
     let input;
     const { classes, size } = this.props;
@@ -163,31 +135,7 @@ handlePriceChange(e) {
         {/* Price Field */}
         <FormControl variant="outlined" style={{marginTop:'40px'}} className={classes.formControl}>
             <Typography className='fieldTitle' variant='display1' gutterBottom>Price (€)</Typography>
-                {/* <Select
-                  value={this.state.price}
-                  price={this.state.price}
-                  onChange={this.handleChange}
-                  className='price'
-                  input={
-                    <OutlinedInput
-                      labelWidth={this.state.labelWidth}
-                      name="price"
-
-                    />
-                  }
-                  className='SelectedMenuItem'
-                >
-                  <MenuItem value="" className="MenuItem">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={100} className="MenuItem">100</MenuItem>
-                  <MenuItem value={200} className="MenuItem" >200</MenuItem>
-                  <MenuItem value={300} className="MenuItem">300</MenuItem>
-                  <MenuItem value={400} className="MenuItem">400</MenuItem>
-                  <MenuItem value={500} className="MenuItem">500</MenuItem>
-                  <MenuItem value={600} className="MenuItem">600</MenuItem>
-                  <MenuItem value={700} className="MenuItem">700</MenuItem>
-                </Select> */}
+              
                     <TextField
                     id="outlined-bare"
                     // className={classes.textField}
@@ -205,30 +153,7 @@ handlePriceChange(e) {
         {/* Size Field */}
         <FormControl variant="outlined"  style={{marginTop:'40px'}} className={classes.formControl}>
             <Typography className='fieldTitle' variant='display1' gutterBottom>Size (m2)</Typography>
-                {/* <Select
-                  value={this.state.size}
-                  onChange={this.handleChange}
-                  input={
-                    <OutlinedInput
-                      labelWidth={this.state.labelWidth}
-                      name="size"
-
-                    />
-                  }
-                  className='SelectedMenuItem'
-                >
-                  <MenuItem value="" className="MenuItem">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={30} className="MenuItem">30+</MenuItem>
-                  <MenuItem value={50} className="MenuItem" >50+</MenuItem>
-                  <MenuItem value={80} className="MenuItem">80+</MenuItem>
-                  <MenuItem value={90} className="MenuItem">90+</MenuItem>
-                  <MenuItem value={100} className="MenuItem">100+</MenuItem>
-                  <MenuItem value={120} className="MenuItem">120+</MenuItem>
-                  <MenuItem value={150} className="MenuItem">150+</MenuItem>
-
-                </Select> */}
+               
         </FormControl>
         <FormControl>
         <TextField
@@ -272,6 +197,28 @@ handlePriceChange(e) {
                   <MenuItem value={'Kentro'} className="MenuItem">Kentro</MenuItem>
                 </Select>
         </FormControl>
+
+
+        {/* Phone Field */}
+        <FormControl variant="outlined"  style={{marginTop:'40px'}} className={classes.formControl}>
+            <Typography className='fieldTitle' variant='display1' gutterBottom>Your Phone</Typography>
+               
+        </FormControl>
+        <FormControl>
+        <TextField
+        id="outlined-bare"
+        // className={classes.textField}
+        type='number'
+        defaultValue={this.state.sizeSubmit}
+        margin="normal"
+        variant="outlined"
+        className='phone'
+        onChange={this.handlePhoneNumber}
+                  
+      />
+        </FormControl>
+
+
       </form>
 
     );
